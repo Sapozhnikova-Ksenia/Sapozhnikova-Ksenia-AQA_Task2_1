d6758b9a1d03eaef52d26c2e451648bb3a373e8d
Sapozhnikova Ksenia <mobillka92@gmail.com>
2022-01-11 20:36:43 +0300

    corrected yml file and corrected json schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -1,7 +1,6 @@
 package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
@@ -18,7 +17,6 @@ class MobileBankApiTestV4 {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-        ;
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
